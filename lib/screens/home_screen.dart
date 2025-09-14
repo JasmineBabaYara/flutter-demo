e6f6ee4e6cb4734cc3266dd21a49d53be7465e2f
JasmineBabaYara <babayarajasmine@gmail.com>
2025-09-14 00:56:48 +0000

    refactor HomeScreen layout to use GridView for item display and update button styles

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -20,14 +20,61 @@ class HomeScreen extends StatelessWidget {
       body: Container(
         child: Column(
           children: [
-            Expanded(child: Container(color: Colors.amber)),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                childAspectRatio: 16 / 9,
+                padding: EdgeInsets.all(10),
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color(0xFF1A191D),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color(0xFF1A191D),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color(0xFF1A191D),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color(0xFF1A191D),
+                    ),
+                  ),
+                ],
+              ),
+            ),
             Container(
               padding: EdgeInsets.all(14),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  TextButton(onPressed: () {}, child: Text('New Reminder')),
-                  TextButton(onPressed: () {}, child: Text('Add List'))
+                  TextButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.add_circle, color: Colors.blue, size: 30),
+                    label: Text(
+                      'New Reminder',
+                      style: TextStyle(color: Colors.blue, fontSize: 15),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Add List',
+                      style: TextStyle(color: Colors.blue, fontSize: 15),
+                    ),
+                  ),
                 ],
               ),
             ),
