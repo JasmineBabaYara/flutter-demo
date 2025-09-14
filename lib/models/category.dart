4f4fb9e1b4481adc4a42c47c78369c56432c8d68
JasmineBabaYara <babayarajasmine@gmail.com>
2025-09-14 00:56:38 +0000

    add Category model with properties and default instance. testing out dsta types

---
+++ lib/models/category.dart
@@ -0,0 +1,9 @@
+ class Category {
+  String id;
+  String name;
+  bool? isChecked;
+
+  Category({required this.id, required this.name, this.isChecked = false});
+ } 
+
+ var category = (id:'1', name:'Category 1');
