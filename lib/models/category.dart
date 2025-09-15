45c4517573ab2e103d9c8941da76889775705865
JasmineBabaYara <babayarajasmine@gmail.com>
2025-09-15 10:08:20 +0000

    refactor Category model to include icon property

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,9 +1,17 @@
- class Category {
+import 'package:flutter_application_1/common.widgets/category_icon.dart';
+
+class Category {
   String id;
   String name;
   bool? isChecked;
+  final CategoryIcon icon;
 
-  Category({required this.id, required this.name, this.isChecked = false});
- } 
+  Category({
+    required this.id,
+    required this.name,
+    this.isChecked = false,
+    required this.icon,
+  });
+}
 
- var category = (id:'1', name:'Category 1');
+var category = (id: '1', name: 'Category 1');
