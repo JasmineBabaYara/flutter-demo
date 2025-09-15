132f7732bec897982433a201ba382161cec9cb2a
JasmineBabaYara <babayarajasmine@gmail.com>
2025-09-15 10:08:06 +0000

    add CategoryIcon widget for displaying category icons with customizable background color

---
+++ lib/common.widgets/category_icon.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CategoryIcon extends StatelessWidget {
+  // const CategoryIcon({super.key});
+
+  final dynamic bgColor;
+  final IconData iconData;
+
+   // ignore: prefer_const_constructors_in_immutables
+   CategoryIcon({super.key, required this.bgColor, required this.iconData});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: bgColor,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Icon(iconData, size: 30, color: Colors.white),
+    );
+  }
+}
