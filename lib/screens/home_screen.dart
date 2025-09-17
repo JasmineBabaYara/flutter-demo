e2d3fde81670d96c9b40317c0f1fb4ac04e57e53
JasmineBabaYara <babayarajasmine@gmail.com>
2025-09-17 16:53:45 +0000

    refactor HomeScreen to support dynamic layout switching between grid and list views

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,24 +5,41 @@ import 'package:flutter_application_1/common.widgets/category_icon.dart';
 import 'package:flutter_application_1/models/category.dart';
 import 'package:flutter_application_1/screens/home/footer.dart';
 import 'package:flutter_application_1/screens/home/grid_view_items.dart';
+import 'package:flutter_application_1/screens/home/list_view_items.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String layoutType = 'list';
 
   CategoryCollection categoryCollection = CategoryCollection();
 
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         actions: [
           TextButton(
-            onPressed: () {},
-            child: Text(
-              'Edit',
-              style: TextStyle(color: Colors.white, fontSize: 20),
-            ),
+            onPressed: () {
+              if(layoutType == 'grid'){
+                setState(() {
+                  layoutType = 'list';
+                });
+              } else {
+                setState(() {
+                  layoutType = 'grid';
+                });
+              }
+  },
+  child: Text(
+    layoutType == 'grid' ? 'Edit' : 'Done',
+    style: TextStyle(color: Colors.white, fontSize: 20),
+  ),
           ),
         ],
       ),
@@ -30,15 +47,15 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             Expanded(
-              child: GridViewItems(categoryCollection: categoryCollection),
+              child: layoutType == 'grid'
+                  ? GridViewItems(categoryCollection: categoryCollection)
+                  : ListViewItems(categoryCollection: categoryCollection),
             ),
             Footer(),
           ],
         ),
       ),
     );
+  
   }
 }
-
-
-
